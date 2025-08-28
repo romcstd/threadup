c8a85f57af1415b692953b4856aa8ebe37c3bcb1
romcstd <romelroxascustodio@gmail.com>
2025-08-29 01:59:36 +0800

    release-v3.0

--- client/src/stores/posts/usePostStore.ts
+++ client/src/stores/posts/usePostStore.ts
@@ -1,8 +1,8 @@
 import { create } from "zustand";
-import axios from "axios";
 import type { Post } from "./types";
-import { getAuthHeaders } from "@/utils/getAuthHeaders";
-const API_URL = import.meta.env.VITE_API_URL + "/api/posts/";
+import api from "@/utils/api";
+
+const API_URL = "/posts/";
 
 interface PostState {
   posts: Post[];
@@ -33,8 +33,7 @@ export const usePostStore = create<PostState>((set) => ({
   fetchPosts: async () => {
     try {
       set({ isLoading: true, isError: false, message: "" });
-      const headers = await getAuthHeaders();
-      const response = await axios.get(API_URL, headers);
+      const response = await api.get(API_URL);
       set({ posts: response.data, isLoading: false });
     } catch (error: any) {
       set({
@@ -49,7 +48,7 @@ export const usePostStore = create<PostState>((set) => ({
   fetchAllPosts: async () => {
     try {
       set({ isLoading: true, isError: false, message: "" });
-      const response = await axios.get(API_URL + "all");
+      const response = await api.get(API_URL + "all");
       set({ posts: response.data, isLoading: false });
     } catch (error: any) {
       set({
@@ -63,7 +62,7 @@ export const usePostStore = create<PostState>((set) => ({
   getPostsByUsername: async (username: string) => {
     set({ isLoading: true, isError: false, message: "" });
     try {
-      const response = await axios.get(API_URL + "user/" + username);
+      const response = await api.get(API_URL + "user/" + username);
       set({ posts: response.data, isLoading: false });
     } catch (error: any) {
       set({
@@ -77,8 +76,7 @@ export const usePostStore = create<PostState>((set) => ({
   createPost: async (postData) => {
     try {
       set({ isLoading: true, isError: false, message: "", actionType: null });
-      const headers = await getAuthHeaders();
-      await axios.post(API_URL, postData, headers);
+      await api.post(API_URL, postData);
       await usePostStore.getState().fetchAllPosts();
       set({
         isLoading: false,
@@ -97,8 +95,7 @@ export const usePostStore = create<PostState>((set) => ({
   updatePost: async (id, postData) => {
     try {
       set({ isLoading: true, isError: false, message: "" });
-      const headers = await getAuthHeaders();
-      const response = await axios.put(API_URL + id, postData, headers);
+      const response = await api.put(API_URL + id, postData);
       set((state) => ({
         posts: state.posts.map((p) => (p._id === id ? response.data : p)),
         isLoading: false,
@@ -116,8 +113,7 @@ export const usePostStore = create<PostState>((set) => ({
   deletePost: async (id) => {
     try {
       set({ isLoading: true, isError: false, message: "", actionType: null });
-      const headers = await getAuthHeaders();
-      await axios.delete(API_URL + id, headers);
+      await api.delete(API_URL + id);
       set((state) => ({
         posts: state.posts.filter((p) => p._id !== id),
         isLoading: false,
