9b8d5c000a218187f24a72826682333c250e1eea
romcstd <romelroxascustodio@gmail.com>
2025-08-29 02:01:58 +0800

    update token expiration

--- server/utils/tokens.js
+++ server/utils/tokens.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 // Access token: short-lived (e.g. 15m) → used for API requests.
 const generateAccessToken = (id) =>
-  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1m' })
+  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '5m' })
 
 //Refresh token: long-lived (e.g. 7d or 30d) → used to request a new access token when the old one expires.
 const generateRefreshToken = (id) =>
