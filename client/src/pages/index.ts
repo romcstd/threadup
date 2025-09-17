70460bdd0fb1669a44ee1e1507e7f64002087aea
romcstd <romelroxascustodio@gmail.com>
2025-09-17 16:03:40 +0800

    Update app layout, Create not found page, profile routing and update serve prod setup

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -2,3 +2,5 @@ export { Home } from './Home';
 export { Login } from './Login';
 export { Register } from './Register';
 export { Profile } from './Profile';
+export { PageNotFound } from './page-not-found';
+export { UserNotFound } from './user-not-found';
--- server/index.js
+++ server/index.js
@@ -18,8 +18,8 @@ connectDB();
 // Enable CORS for dev and production frontends
 app.use(cors({
   origin: [
-    'http://localhost:5137',               // Vite dev server
-    'https://threadup-app.vercel.app',   // Vercel frontend
+    'http://localhost:5137', // Vite Development
+    'https://threadup-app.vercel.app',   // Vercel Production
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true, // allows sending cookies / auth headers
@@ -39,19 +39,13 @@ app.get('/api/health', (req, res) => {
 app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
 
-// Serve frontend
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')
-    )
-  );
-} else {
-  app.get('/', (req, res) => res.send(`Server running on http://localhost:${process.env.PORT || 5000}`));
-}
+app.get('/', (req, res) =>
+  res.send(`Server running on http://localhost:${process.env.PORT || 5000}`)
+);
 
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-// app.listen(port, () => console.log(`Server started on port http://localhost:${port}`));
-app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () =>
+  console.log(`Server running on port http://localhost:${PORT}`)
+);
